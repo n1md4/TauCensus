527e3b6df899a9bc05e3c085ebb9327fef45e3f1
n1md4 <jon@whiteheat.org.uk>
2015-12-22 18:06:24 +0000

    Update manageApp.py

--- manageApp.py
+++ manageApp.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 from flask.ext.script import Manager, Shell, Server
-from FlaskApp import app
+from TauCensus import app
 
 manager = Manager(app)
 
 @manager.command
 def createdb():
-    from FlaskApp.models import db
+    from TauCensus.models import db
     db.create_all()
 
 manager.run()
